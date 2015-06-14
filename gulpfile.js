669d3a8872a420448a8ed7d7099baec3fa47fffb
Dominic Vogl <dv@bergwerk.ag>
2015-06-14 18:27:47 +0200

    [task] update structure

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,14 @@ var gulp = require('gulp'),
    watch = require('gulp-watch'),
    plumber = require('gulp-plumber'),
    rename = require("gulp-rename"),
-//compass = require('gulp-compass'),
+   //compass = require('gulp-compass'),
    autoprefixer = require("gulp-autoprefixer");
 
 // Compile SASS files
 gulp.task('styles', function () {
 
    // Foundation 5
-   gulp.src(['scss/app.scss'])
+   gulp.src(['src/scss/app.scss'])
       .pipe(plumber())
       .pipe(sass({outputStyle: 'expanded'}))
       .pipe(gulp.dest('css'))
@@ -32,36 +32,36 @@ gulp.task('styles', function () {
 var filelist = [
 
    // Foundation Stuff
-   //'js/foundation/foundation.js',
-   //'js/foundation/foundation.abide.js',
-   //'js/foundation/foundation.accordion.js',
-   //'js/foundation/foundation.alert.js',
-   //'js/foundation/foundation.clearing.js',
-   //'js/foundation/foundation.dropdown.js',
-   //'js/foundation/foundation.equalizer.js',
-   //'js/foundation/foundation.interchange.js',
-   //'js/foundation/foundation.joyride.js',
-   //'js/foundation/foundation.magellan.js',
-   //'js/foundation/foundation.offcanvas.js',
-   //'js/foundation/foundation.orbit.js',
-   //'js/foundation/foundation.reveal.js',
-   //'js/foundation/foundation.slider.js',
-   //'js/foundation/foundation.tab.js',
-   //'js/foundation/foundation.tooltip.js',
-   //'js/foundation/foundation.topbar.js',
+   //'src/js/foundation/foundation.js',
+   //'src/js/foundation/foundation.abide.js',
+   //'src/js/foundation/foundation.accordion.js',
+   //'src/js/foundation/foundation.alert.js',
+   //'src/js/foundation/foundation.clearing.js',
+   //'src/js/foundation/foundation.dropdown.js',
+   //'src/js/foundation/foundation.equalizer.js',
+   //'src/js/foundation/foundation.interchange.js',
+   //'src/js/foundation/foundation.joyride.js',
+   //'src/js/foundation/foundation.magellan.js',
+   //'src/js/foundation/foundation.offcanvas.js',
+   //'src/js/foundation/foundation.orbit.js',
+   //'src/js/foundation/foundation.reveal.js',
+   //'src/js/foundation/foundation.slider.js',
+   //'src/js/foundation/foundation.tab.js',
+   //'src/js/foundation/foundation.tooltip.js',
+   //'src/js/foundation/foundation.topbar.js',
 
    // Your Own Stuff
-   'js/custom/fastclick.js',
-   'js/custom/jquery.min.js',
-   //'js/custom/modernizr.js',
-   'js/custom/setup.js'
+   'src/js/custom/fastclick.js',
+   'src/js/custom/jquery.min.js',
+   //'src/js/custom/modernizr.js',
+   'src/js/custom/setup.js'
 ];
 
 // Concatenate & Minify JS
 gulp.task('scripts', function () {
    gulp.src(filelist)
       .pipe(plumber())
-      .pipe(concat('build/app.js'))
+      .pipe(concat('/app.js'))
       .pipe(gulp.dest('js'))
       .pipe(uglify())
       .pipe(rename({suffix: '.min'}))
